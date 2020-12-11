8aa981b96bbd9d580e1680e864702934cb6e8a3a
Evelyn Szarek <evelynszarek@gmail.com>
2020-12-11 18:26:34 +0000

    set up member_controller

---
+++ controllers/member_controller.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+from models.member import Member
+import repositories.member_repository as member_repository
+import repositories.activity_repository as activity_repository
+
+members_blueprint = Blueprint("members", __name__)
+
+@members_blueprint.route("/members")
+def members():
+    members = member_repository.select_all()
+    return render_template("members/index.html", members = members)
+
+@members_blueprint.route("/members/<id>")
+def show(id):
+    member = member_repository.select(id)
+    activities = member_repository.activities(member)
+    return render_template("members/show.html", member = member, activities = activities)
+
+    
