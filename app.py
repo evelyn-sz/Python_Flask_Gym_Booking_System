9d78ee95371e2df54abbd090d2353fbeadc4ad61
Evelyn Szarek <evelynszarek@gmail.com>
2020-12-11 18:17:06 +0000

    set up app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+from controllers.booking_controller import bookings_blueprint
+from controllers.activity_controller import activities_blueprint
+from controllers.member_controller import members_blueprint
+
+app = Flask(__name__)
+
+app.register_blueprint(bookings_blueprint)
+app.register_blueprint(activities_blueprint)
+app.register_blueprint(members_blueprint)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
