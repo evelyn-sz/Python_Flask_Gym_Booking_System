19e829b5f8c479a02a318b3ff03d7c27436e22f1
Evelyn Szarek <evelynszarek@gmail.com>
2020-12-11 21:19:02 +0000

    added save, select_all, delete_all and delete(id) in booking_repository

---
+++ repositories/booking_repository.py
@@ -0,0 +1,33 @@
+from db.run_sql import run_sql
+from models.activity import Activity
+from models.member import Member 
+from models.booking import Booking 
+
+def save(booking):
+    sql = "INSERT INTO bookings (member_id, activity_id) VALUES (%s) RETURNING id"
+    values = (booking.member.id, booking.activity.id)
+    results = run_sql(sql, values)
+    booking.id = result[0]['id']
+    return booking
+
+def select_all():
+    bookings = []
+
+    sql = "SELECT * FROM bookings"
+    results = run_sql(sql)
+
+    for row in results:
+        member = member_repository.select(row['member_id'])
+        activity = activity_repository.select(row['activity_id'])
+        booking = Booking(member, activity, row['id'])
+        bookings.append(booking)
+    return bookings
+
+def delete_all():
+    sql = "DELETE FROM bookings"
+    run_sql(sql)
+
+def delete(id):
+    sql = "DELETE FROM bookings WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
